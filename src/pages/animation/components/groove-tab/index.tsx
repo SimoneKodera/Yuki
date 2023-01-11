76c97a1692575c2ea3e3c306a342d068630f7f4f
王靖文 <wangjingwen@datagrand.com>
2023-01-11 15:13:32 +0800

    tink audio import failed

--- src/pages/animation/components/groove-tab/index.tsx
+++ src/pages/animation/components/groove-tab/index.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './index.scss';
 import cs from 'classnames';
+import tink from '../../../../audio/tink.wav';
 
 export const GrooveTab = () => {
     const [tabIndex, setTabIndex] = useState(1);
 
     return (
         <section className="animation-box groove-tab">
+            <audio src="/Users/jingwen/Desktop/tink.wav" id='tink'></audio>
             <div className="tab-wrapper">
                 {[0,0,0].map((v, idx) => (
                     <div
@@ -16,7 +18,15 @@ export const GrooveTab = () => {
                         'tab-last': idx === tabIndex - 1,
                         'tab-next': idx === tabIndex + 1,
                         })}
-                        onClick={() => setTabIndex(idx)}
+                        onClick={() => {
+                            setTabIndex(idx);
+                            // setTimeout(() => {
+                            //     audioRef.current.currentTime = 0;
+                            //     audioRef.current.play();
+                            // }, 500)
+                            const tinkEl = document.getElementById('tink') as any;
+                            tinkEl.play();
+                        }}
                     >
                         <div className="tab-inner"></div>
                     </div>
