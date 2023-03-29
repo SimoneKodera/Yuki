fe405536fb34c9716b27d6d447be2838abbb15e3
王靖文 <wangjingwen@datagrand.com>
2023-03-29 11:47:43 +0800

    更换主题颜色; 表格整体居中;

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    {/* 参考 SeedToken */}
+    <ConfigProvider theme={{
+      token: {
+        colorPrimary: 'pink',
+        colorBgTextHover: '#ffc0cb3d'
+      }
+    }}>
+      <App />
+    </ConfigProvider>
   </BrowserRouter>
 );
 
--- src/pages/all/index.tsx
+++ src/pages/all/index.tsx
@@ -1,47 +1,72 @@
-import { Table } from "antd";
+import { Table, Space, TableColumnProps } from "antd";
 import { useEffect, useState } from "react";
 import axios from "../../http/index";
 import './index.scss';
+import {
+  DeleteOutlined
+} from '@ant-design/icons';
+
+interface ColumnType extends TableColumnProps<any> {
+  align?: 'center';
+}
 
 export const AllPage = () => { 
   const [data, setData] = useState<any>([]);
 
-  const columns = [
+  const columns: ColumnType[] = [
     {
       title: 'Nirakana',
       key: 'nirakana',
-      dataIndex: 'nirakana'
+      dataIndex: 'nirakana',
+      align: 'center',
     },
     {
       title: 'Tone',
       key: 'tone',
-      dataIndex: 'tone'
+      dataIndex: 'tone',
+      align: 'center',
     },
     {
       title: 'Word',
       key: 'word',
-      dataIndex: 'word'
+      dataIndex: 'word',
+      align: 'center',
     },
     {
       title: 'Type',
       key: 'type',
-      dataIndex: 'type'
+      dataIndex: 'type',
+      align: 'center',
     },
     {
       title: 'Meaning',
       key: 'meaning',
-      dataIndex: 'meaning'
+      dataIndex: 'meaning',
+      align: 'center',
     },
     {
       title: 'Lesson Number',
       key: 'lesson_number',
-      dataIndex: 'lesson_number'
+      dataIndex: 'lesson_number',
+      align: 'center',
     },
     {
       title: 'Example',
       key: 'example_sentence',
-      dataIndex: 'example_sentence'
+      dataIndex: 'example_sentence',
+      align: 'center',
     },
+    {
+      title: 'Action',
+      key: 'action',
+      dataIndex: '',
+      align: 'center',
+      render: () => ( 
+        // <Space>
+          <DeleteOutlined />
+        // </Space>
+      )
+    }
   ]
 
   useEffect(() => {
@@ -55,7 +80,9 @@ export const AllPage = () => {
     <div className="all-page">
       <br />
       <section className="table-wrapper">
-        <Table columns={columns} dataSource={data} rowKey='id'></Table>
+        <Table columns={columns} dataSource={data} rowKey='id' pagination={{
+          showSizeChanger: false
+        }}></Table>
       </section>
     </div>
   )
