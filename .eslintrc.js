96be20a633476c7b9693baf5bf1f83474ada4403
王靖文 <wangjingwen@datagrand.com>
2023-09-10 11:21:45 +0800

    remove lint

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [require.resolve('@bixi-design/lint/eslint')],
-  rules: {
-    '@typescript-eslint/no-explicit-any': ['warn'],
-    indent: ['off']
-  }
-};
--- .stylelintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: [require.resolve('@bixi-design/lint/stylelint')],
-};
